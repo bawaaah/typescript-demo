d1f3f5466e31afa47e9377f70058ed912ab0a275
Ranu20001 <it22168436@my.sliit.lk>
2024-07-23 21:10:59 +0530

    DAY 01

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello")
+import {v4 as uuidV4} from 'uuid'
+
+
+type task = {
+    id:string,
+    title:string,
+    completed:boolean,
+    createdAt:Date
+}
+
+const taskList = document.querySelector<HTMLUListElement>('#list')
+const form = document.getElementById("task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-list")
+// const tasks = Task[] = loadTask()
+// tasks.forEach(addListItem)
