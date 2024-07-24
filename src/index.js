472a9e7edc42823aeb0f5e1f5c0effecb951c007
Ranu20001 <it22168436@my.sliit.lk>
2024-07-24 14:58:50 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+"use strict";
+const input = document.getElementById("input-text");
+const submit_form = document.getElementById("task-form");
+const task_list = document.getElementById("list");
+function addTask(task) {
+    const listItem = document.createElement('li');
+    listItem.className = "m-1";
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'm-1';
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
+    taskText.className = 'm-1';
+    const delete_btn = document.createElement('button');
+    delete_btn.textContent = 'DELETE';
+    delete_btn.className = 'border-solid border-slate-950 border-2 rounded';
+    delete_btn.addEventListener('click', (e) => {
+        task_list.removeChild(listItem);
+    });
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskText);
+    listItem.appendChild(delete_btn);
+    task_list.appendChild(listItem);
+}
+submit_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const task = input.value.trim();
+    if (task) {
+        addTask(task);
+        input.value = '';
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,47 @@
-import {v4 as uuidV4} from 'uuid'
+const input = document.getElementById("input-text") as HTMLInputElement
+const submit_form = document.getElementById("task-form") as HTMLFormElement
+const task_list = document.getElementById("list") as HTMLDListElement 
 
+function addTask(task: string): void{
+    const listItem = document.createElement('li')
+    listItem.className = "m-1"
 
-type task = {
-    id:string,
-    title:string,
-    completed:boolean,
-    createdAt:Date
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.className = 'm-1'
+
+    const taskText = document.createElement('span')
+    taskText.textContent = task
+    taskText.className = 'm-1'
+
+    const delete_btn = document.createElement('button')
+    delete_btn.textContent = 'DELETE'
+    delete_btn.className = 'border-solid border-slate-950 border-2 rounded'
+    delete_btn.addEventListener('click', (e) => {
+        task_list.removeChild(listItem)
+    })
+
+    listItem.appendChild(checkbox)
+    listItem.appendChild(taskText)
+    listItem.appendChild(delete_btn)
+    
+    task_list.appendChild(listItem)
 }
 
-const taskList = document.querySelector<HTMLUListElement>('#list')
-const form = document.getElementById("task-form") as HTMLFormElement | null
-const input = document.querySelector<HTMLInputElement>("#new-task-list")
-// const tasks = Task[] = loadTask()
-// tasks.forEach(addListItem)
+submit_form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const task = input.value.trim()
+    if(task){
+        addTask(task)
+        input.value=''
+    }
+})
+
+
+
+
+
+
+
+
+
